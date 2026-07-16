bf094c476bae79aeadb7bdc2b3651e483d949208
Hafiz-Fast <arhamzeeshan617@gmail.com>
2026-07-16 19:24:45 +0500

    Added Stimulus Target

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,11 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["message"]
+
   connect() {
     console.log("Hello Controller Connected!")
   }
 
-  greet() {
-    alert("Welcome to PropertyHub!")
+  // greet(event) {
+  //   console.log(event)
+  //   alert("Welcome to PropertyHub!")
+  // }
+
+  greet(){
+    this.messageTarget.textContent = "Welcome to PropertyHub!"
   }
 }
--- app/javascript/theme.js
+++ app/javascript/theme.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("explore-btn");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const button = document.getElementById("explore-btn");
 
-  button.addEventListener("click", () => {
-    // alert("Welcome to PropertyHub!");
-    button.textContent = "Loading...";
-    button.style.backgroundColor = "green";
-  });
-});
+//   button.addEventListener("click", () => {
+//     // alert("Welcome to PropertyHub!");
+//     button.textContent = "Loading...";
+//     button.style.backgroundColor = "green";
+//   });
+// });
