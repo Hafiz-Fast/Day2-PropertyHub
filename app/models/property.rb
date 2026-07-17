8a7c59f19b40197241d6d0782f880ee2647dd882
Hafiz-Fast <arhamzeeshan617@gmail.com>
2026-07-17 18:14:28 +0500

    Callback learnt and added

--- app/models/property.rb
+++ app/models/property.rb
@@ -9,11 +9,17 @@ class Property < ApplicationRecord
     
     validate :price_must_be_even
 
+    before_save :capitalize_title
+
     private
     
     def price_must_be_even
         if price.present? && price.odd?
             errors.add(:price, "must be an even number")
         end
+    end
+
+    def capitalize_title
+        self.title = title.titleize
     end
 end
